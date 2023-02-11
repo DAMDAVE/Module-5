c4062f5b2ae6e35805d7f802c78acc066c9fa962
DAMDAVE <121645121+DAMDAVE@users.noreply.github.com>
2023-02-11 21:28:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -96,9 +96,9 @@ function buildAndShowHomeHTML (categories) {
 
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (responseText) {
-      document.querySelector("#main-conetent").innerHTML = responseText
-
+    function (homeHtml) {
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name
+      
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
